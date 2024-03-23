33c291f18978e16d47c1cf6c2fe22b5f2b5254a6
GUILHERME DILIO DE SOUZA <1091392413037@fatec-srv.local>
2024-03-23 10:32:31 -0300

    (23/03) Término do primeiro CRUD (Cliente)

--- back-end/src/controllers/cliente.js
+++ back-end/src/controllers/cliente.js
@@ -17,4 +17,61 @@ controller.create = async function (req, res) {
     }
 }
 
+controller.retrieveAll = async function (req, res) {
+    try {
+        const result = await Cliente.find().sort({nome: 'asc' })
+        // HTTP 200: OK (implicito)
+        res.send(result)
+    }
+    catch(error) {
+        console.error(error)
+        //HTTP 500: Internal Server Error
+        res.status(500).end()
+    }
+}
+
+controller.retrieveOne = async function (req, res) {
+    try {
+        const result = await Cliente.findById(req.params.id)
+        // Documento encontrado -> HTTP 200: OK (implícito)
+        if (result) res.send(result)
+        // Documento não encontrado -> HTPP 404: NOT FOUND
+        else res.status(404).end()
+    }
+    catch(error) {
+        console.error(error)
+        //HTTP 500: Internal Server Error
+        res.status(500).end()
+    }
+}
+
+controller.update = async function (req, res) {
+    try {
+        const result = await Cliente.findByIdAndUpdate(req.params.id, req.body)
+        // Documento encontrado e atualizado -> HTTP 204: No Content
+        if (result) res.status(204).end()
+        // Documento não encontrado (e não atualizado) -> HTTP 404: Not Found
+        else res.status(404).end()
+    }
+    catch(error) {
+        console.error(error)
+        //HTTP 500: Internal Server Error
+        res.status(500).end()
+    }
+}
+
+controller.delete = async function (req, res) {
+    try {
+        const result = await Cliente.findByIdAndDelete(req.params.id)
+        // Documento encontrado e excluído -> HTTP 204: No Content
+        if (result) res.status(204).end()
+        // Documento não encontrado (e não excluído) -> HTTP 404: Not Found
+        else res.status(404).end()
+    }
+    catch(error) {
+        console.error(error)
+        //HTTP 500: Internal Server Error
+        res.status(500).end()
+    }
+}
 export default controller
